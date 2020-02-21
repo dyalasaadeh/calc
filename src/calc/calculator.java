52d44e6a5991e05121fbb3e9feb591809988cdcf
USER <USER@3578-i7-SSD-8th>
2020-02-21 12:59:11 +0200

    calculator with addition

---
+++ src/calc/calculator.java
@@ -0,0 +1,13 @@
+package calc;
+
+public class calculator {
+
+	public static void main(String[] args) {
+	System.out.println(add(3,4));
+	}
+	
+	public static int add (int x, int y) {
+		return x+y;
+			}
+
+}
